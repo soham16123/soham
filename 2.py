9963ceb41c1fbd8ef1c3f4445c5a935cc51479b0
soham16123 <106761393+soham16123@users.noreply.github.com>
2022-08-01 20:55:15 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,14 @@
+person = {
+    
+}
+print(type(person))
+
+
+person = {
+    "name": "Soham",
+    "mobile": 9876543211,
+    "E-mail": "gbalasaheb160@gmail.com",
+    "address": "tulsi harmony sector 1 khanda colonypython"
+}  
+print(person["name"])
+
